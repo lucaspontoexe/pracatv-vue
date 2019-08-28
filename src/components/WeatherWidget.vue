51d7417986ac7a64da9fc6a9b45d89b281fdff85
lucaspontoexe <ltx16@hotmail.com>
2019-08-28 20:07:30 -0300

    begin styling weather widget

--- src/components/WeatherWidget.vue
+++ src/components/WeatherWidget.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="weather-widget">
-    <div class="top"></div>
-    <div class="bottom"></div>
+    <div class="top">
+      <div class="icon">
+
+      </div>
+      <div class="temperature">
+        <span class="text">22°</span>
+      </div>
+    </div>
+    <div class="bottom">
+      <span>Qualquer cidade</span>
+    </div>
   </div>
 </template>
 
@@ -14,6 +23,7 @@ export default {
 <style lang="scss">
 @import "./variables.scss";
 @import "./fonts.scss";
+$opaqueblue: rgb(2, 32, 60);
 
 .weather-widget {
   position: absolute;
@@ -21,27 +31,53 @@ export default {
   top: $mainPosition;
   width: 232px;
 
-  background-color: $darkblue;
+  // background-color: $opaqueblue;
 }
 
 .top {
   position: relative;
   height: 70px;
-  background-color: lighten($color: $darkblue, $amount: 0.2);
+  // clip-path: inset(0% 50% 0% 0%);
+
+  .icon {
+    position: absolute;
+    width: 50%;
+    height: 100%;
+    background-color: darken($color: $opaqueblue, $amount: 5);
+  }
+
+  .temperature {
+    position: absolute;
+    left: 50%;
+    top: 0%;
+    width: 50%;
+    height: 100%;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    background-color: $opaqueblue;
+
+    .text {
+      color: $white;
+      font-family: "Futura Light";
+      font-size: 54px;
+    }
+  }
 }
 
 .bottom {
   position: relative;
   height: 45px;
-  background-color: lighten($color: $darkblue, $amount: 0.3);
+  background-color: lighten($color: $opaqueblue, $amount: 8);
   text-align: center;
   margin: 0px;
-}
 
-span {
-  line-height: 45px;
-  font-size: 30px;
-  color: $white;
-  font-family: "Futura Heavy", "Century Gothic", Arial, Helvetica, sans-serif;
+  span {
+    line-height: 45px;
+    color: $white;
+    font-family: "Futura Light", "Century Gothic", Arial, Helvetica, sans-serif;
+  }
 }
 </style>
